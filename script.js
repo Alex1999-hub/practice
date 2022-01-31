473c8523291403c3c19c5ae2f2413aeff6029d9c
Alex <example@gmail.com>
2022-01-31 21:52:07 +0200

    lesson cycles

--- script.js
+++ script.js
@@ -10,12 +10,31 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько вы его оцените?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько вы его оцените?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0;  i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько вы его оцените?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 10) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count > 0 && personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман');
+} else if (personalMovieDB.count == 0) {
+    console.log('Произошла ошибка');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
